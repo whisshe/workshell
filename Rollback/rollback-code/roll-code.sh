b09288987cae5a5900d3ce35c81c1891b14f5cac
pc@work1 <pc@work1>
2018-03-28 17:38:07 +0800

    code回退脚本

--- Rollback/rollback-code/roll-code.sh
+++ Rollback/rollback-code/roll-code.sh
@@ -47,7 +47,7 @@ while [ True ];do
 		        #到对应机器的bak目录解压，然后替换掉项目目录
 		        for M in $dest_machines;do
 			        ssh game@$M "cd $dest_directory;tar zxf $RollbackVersion;rm -fr ../$name;mv $name ../"
-			        RollbackStatus=`ssh game@$M "[ ! -d $dest_directory/../$name] || 
+			        ssh game@$M "[ ! -d $dest_directory/../$name] || 
 			        echo -e "\033[34m##################"
 			        echo  " $M $name已经回退至$RollbackVersion版本 "
 			        echo -e "##################\033[0m"  "
