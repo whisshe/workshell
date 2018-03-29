151014d91252db009377b8c113fdfe8d00720fb0
test <test>
2018-03-29 18:03:32 +0800

    rollback-game

--- Rollback/rollback-game/roll-game.sh
+++ Rollback/rollback-game/roll-game.sh
@@ -15,7 +15,42 @@ function copyright(){
 function underline(){
     echo "-----------------------------------------"
 }
+front_or_server(){
+    if [ `echo $1|grep qd|wc -l` -eq 1 ];then
+	game.id=front
+    else
+	game.id=server
+    fi
+}
 
+front_roll(){
+	#到对应机器删掉目前的软连接，然后替换成指定版本的项目目录
+	ssh $user@$dest_machine "cd $dest_gameroot_directory;rm $dest_game_directory;ln -s $dest_directory/$name-$gameDate ./$dest_game_directory"
+	rollStatus=`ssh $user@$dest_machine "[ ! -d $dest_gameroot_directory/$dest_game_directory ] || echo yes"` 
+	if [[ $rollStatus == yes ]];then
+	    color 6 "##################"
+	    color 6 "$dest_machine $name已经回退至$gameDate版本"
+	    color 6 "##################" 
+	else 
+	    color 1 "$dest_machine $name回退的目录错误"
+	fi
+}
+
+server_roll(){
+	#到对应机器删掉目前的软连接，然后替换成指定版本的项目目录,重启游戏后端
+	ssh $user@$dest_machine "cd $dest_gameroot_directory/$dest_game_directory;./stopgame"
+	ssh $user@$dest_machine "cd $dest_gameroot_directory;rm $dest_game_directory;ln -s $dest_directory/$name-$gameDate ./$dest_game_directory"
+	rollStatus=`ssh $user@$dest_machine "[ ! -d $dest_gameroot_directory/$dest_game_directory ] || echo yes"` 
+	if [[ $rollStatus == yes ]];then
+	    ssh $user@$dest_machine "cd $dest_gameroot_directory/$dest_game_directory;./startgame"
+	    ssh $user@$dest_machine "cd $dest_gameroot_directory/$dest_game_directory;./checkgame"
+	    color 6 "##################"
+	    color 6 "$dest_machine $name已经回退至$gameDate版本"
+	    color 6 "##################" 
+	else 
+	    color 1 "$dest_machine $name回退的目录错误"
+	fi
+}
 function main(){
 
 while [ True ];do
@@ -31,6 +66,8 @@ while [ True ];do
     dest_machine=$(awk -v num=$number 'BEGIN {FS="%"} {if($1 == num) {print $3}}' $res)
     dest_directory=$(awk -v num=$number 'BEGIN {FS="%"} {if($1 == num) {print $4}}' $res)
     user=$(awk -v num=$number 'BEGIN {FS="%"} {if($1 == num) {print $5}}' $res)
+    dest_gameroot_directory=$(awk -v num=$number 'BEGIN {FS="%"} {if($1 == num) {print $6}}' $res)
+    dest_game_directory=$(awk -v num=$number 'BEGIN {FS="%"} {if($1 == num) {print $7}}' $res)
 
     case $number in
         [0-9]|[0-9][0-9])
@@ -42,22 +79,11 @@ while [ True ];do
         color 4 填入后六位选择回退的版本 
         read gameVersion
 	gameDate=`cat $name.version|grep $gameVersion|awk -F$name- '{print $2}'`
-	gameVersionNum=`cat $name.version| grep $gameVersion|wc -l`
+	gameVersionNum=`cat $name.version| grep $name-$gameDate|wc -l`
 	if [ $gameVersionNum -eq 1 ];then
-            case $gameVersion in 
-                [0-9]?????)
-		    #到对应机器删掉目前的软连接，然后替换成指定版本的项目目录
-			ssh $user@$M "cd $dest_game_directory;rm $name;ln -s $dest_directory/$name-$gameDate ./$name"
-			rollStatus=`ssh $user@$M "[ ! -d $dest_directory/../$name ] || echo yes"` 
-			if [[ $rollStatus == yes ]];then
-			    color 6 "##################"
-			    color 6 "$M $name已经回退至$gameDate版本"
-			    color 6 "##################" 
-			else 
-			    color 1 "$M $name回退的目录错误"
-			fi
-		    ;;
-	    esac
+            front_or_server
+	    [ $game.id == front ] || server_roll
+	    front_roll
 	else	
 	    color 1 "$gameVersion is Wrong"
 	fi
